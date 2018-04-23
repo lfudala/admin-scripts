d604efb90486c7b6376d439edfdbc83d234c974c
Lenard Fudala <lenard.fudala@gmail.com>
2018-04-23 16:57:22 -0500

    Add Cloudflare dns record dump bash script

---
+++ dns/cf-domain-records.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Requires the cfcli command provided by the node
+# cloudflare-cli package available at
+# https://github.com/danielpigott/cloudflare-cli
+# This script assumes that ~/cfcli.yml is configured
+# with multi account profiles
+
+# Set
+domainprofile=$1
+outfile=$(printf "%s-cf-records-%s.csv" "$domainprofile" "$(date +%Y%m%dT%H%M%S)")
+
+# Get domains
+mapfile -t domains < <(cfcli -u $domainprofile zones -f csv)
+
+echo "Type,Name,Value,TTL,Active" > $outfile
+
+for domain in "${domains[@]}"
+do
+  d=$(echo $domain | cut -f 1 -d ',');
+  cfcli -u $domainprofile -d $d list -f csv >> $outfile;
+done
